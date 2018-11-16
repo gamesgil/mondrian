8077ae6fe4b8bbfce2406938180ef362ba5429e7
DESKTOP-RVTL3QT\GilS <games.gil@gmail.com>
2018-11-16 23:53:25 +0200

    almost there

--- script/main.js
+++ script/main.js
@@ -3,6 +3,7 @@ const occupied = []
 const width = 10
 const height = 10
 const maxSize = 4
+const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'beige', 'cyan']
 
 const drawTable = _ => {
     const table = document.createElement('table')
@@ -44,39 +45,65 @@ const createRegion = _ => {
     }
 
     if (foundCell > -1) {
-        let xSteps = 0
-        let cellClass = ''
         const maxSteps = getRandom(1, maxSize)
+        const localOccupied = []
 
-        // break right
-        while(xSteps < maxSteps && ((foundCell + xSteps) % width) < width) {
-            occupied.push(foundCell + xSteps)
 
-            if (xSteps) {
-                if ((foundCell + xSteps) % width) {
-                    getCell(foundCell + xSteps).classList.add('open-left')
+        for (let i = 0; i < maxSteps; i++) {
+            let xSteps = 0
+    
+            // break right
+            while(xSteps < maxSteps && ((foundCell + xSteps) % width) < width) {
+                if (occupied.includes(foundCell + xSteps) || foundCell + xSteps >= width * height) {
+                    return
                 }
-            } else if (foundCell % width && !occupied.includes(foundCell - 1)) {
-                getCell(foundCell + xSteps).classList.add('open-left')                
+
+                localOccupied.push(foundCell + xSteps)
+    
+                if ((foundCell + xSteps) % width) {
+                    if ((foundCell + xSteps) % width && !occupied.includes(foundCell + xSteps - 1)) {
+                        getCell(foundCell + xSteps).classList.add('open-left')                
+                    }
+                } 
+                
+                if (((foundCell + xSteps) % width) < width - 1 &&
+                    (xSteps < maxSteps - 1) &&
+                    !occupied.includes((foundCell + xSteps + 1))) {
+                    getCell(foundCell + xSteps).classList.add('open-right')
+                }
+
+                if (i) {
+                    getCell(foundCell + xSteps).classList.add('open-top')
+                }
+
+                if (i < maxSize - 1) {
+                    if (Math.floor((foundCell + xSteps) / width) < height - 1) {
+                        getCell(foundCell + xSteps).classList.add('open-bottom')
+                    }
+                }
+    
+                getCell(foundCell).innerHTML = xSteps + ',' + maxSteps
+
+                getCell(foundCell + xSteps).style.background = colors[0]
+
+                xSteps++
             }
-            
-            
-            if (((foundCell + xSteps) % width) < width - 1) {
-                getCell(foundCell + xSteps).classList.add('open-right')
-            } 
 
-
-            xSteps++
+            foundCell += width
         }
+
+        while (localOccupied.length) {
+            occupied.push(localOccupied.pop())
+        }
+
+        colors.unshift(colors.pop())
     }
 }
 
 drawTable()
 
 
-createRegion()
-createRegion()
-createRegion()
-createRegion()
+for (let i = 0; i < 150; i++)
+    createRegion()
 
 console.log(occupied)
